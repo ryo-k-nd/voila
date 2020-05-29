276cec61a90879642d17b72d2db11d0774d44f56
Ryo Konishi <konishi@newsdigest.de>
2020-05-29 22:46:55 +0200

    no message

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,40 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+const path = require(`path`);
+const slash = require(`slash`);
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  // we use the provided allContentfulBlogArticle query to fetch the data from Contentful
+  return graphql(
+    `
+      {
+        allContentfulBlogArticle {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+      if (result.errors) {
+        console.log("Error retrieving contentful data",      result.errors);
+      }
+      // Resolve the paths to our template
+      const blogArticleTemplate = path.resolve("./src/templates/post.js");
+      // Then for each result we create a page.
+      result.data.allContentfulBlogArticle.edges.forEach(edge => {
+        createPage({
+          path: `/post/${edge.node.slug}/`,
+          component: slash(blogArticleTemplate),
+          context: {
+      slug: edge.node.slug,
+            id: edge.node.id
+          }
+        });
+      });
+    })
+    .catch(error => {
+      console.log("Error retrieving contentful data", error);
+    });
+};
