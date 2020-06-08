af122b37fe4d0fc61e81496daaaa60eeb83661b5
Ryo Konishi <konishi@newsdigest.de>
2020-06-08 15:51:24 +0200

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,8 +33,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.YOUR_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.GATSBY_YOUR_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
