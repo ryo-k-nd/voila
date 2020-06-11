ad9f925329dd1372ba713a8f16b7827738766cbe
Ryo Konishi <konishi@newsdigest.de>
2020-06-11 16:36:21 +0200

    no message

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,7 +6,10 @@ import BannerSquare from '../images/top/bannerSquare.gif'
 
 const Sidebar = () => (
 		<aside className="sidebar">
-		<img src={BannerSquare} alt="BannerSquare" />
+		  <img src={BannerSquare} alt="BannerSquare" />
+      <p>人気記事</p>
+      <p>ピックアップ記事</p>
+      <p>話題のタグ</p>
 		</aside>
 )
 
