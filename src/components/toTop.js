78fbe99a5e7070c6f47ea317af3b08bc1166a6a1
mito-newsdigeset <mito@news-digest.co.uk>
2020-08-18 11:39:07 +0200

    2020/08/18 Topへ戻るボタンをちょっとスクロールしてから表示されるように変更

--- src/components/toTop.js
+++ src/components/toTop.js
@@ -1,14 +1,44 @@
-import React, { useState, useEffect, useRef, Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
-class toTop extends Component {
+function ToTop() {
+    // useState(初期値)
+    // useState（）は[state, setState]の配列を返してくれるので、それぞれに好きな名前を与える。
+    const [onTop, setOnTop] = useState(true)
 
-    render() {
-        return (
-            <a className="toTop" href="#" >
-                <i class="fas fa-chevron-up"></i>
-            </a >
-        )
+    // scroll時に上部からのオフセットによってstateを更新する
+    function onScroll() {
+        const offset =
+            window.pageYOffset ||
+            (document.documentElement && document.documentElement.scrollTop) ||
+            document.body.scrollTop
+
+        if (offset >= 200 && !onTop) return false
+        if (offset >= 200 && onTop) {
+            setOnTop(false)
+            return false
+        }
+        setOnTop(true)
     }
+
+    function smoothScroll(target) {
+        const { top } = target.getBoundingClientRect()
+        window.scrollTo({
+            top: top + window.pageYOffset,
+            behavior: "smooth"
+        });
+    }
+
+    // 今までならcomponentXXXmount系のライフサイクルでやってたeventListenerの登録などをuseEffectでやる
+    useEffect(() => {
+        document.addEventListener('scroll', onScroll)
+        return () => document.removeEventListener('scroll', onScroll)
+    })
+
+    return (
+        <a className={onTop ? 'toTop' : 'toTop is-show'} href="#" >
+            <i class="fas fa-chevron-up"></i>
+        </a>
+    )
 }
 
-export default toTop
+export default ToTop
